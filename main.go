f8721742838dbc0c4fe6b068b7cf1584ce5014d3
Paulo <pauloeduardo2709@gmail.com>
2020-11-17 02:24:00 -0300

    Add basic repo cloner

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/Pauloo27/tldr/repo"
+	"github.com/Pauloo27/tldr/utils"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	home, err := os.UserHomeDir()
+	utils.HandleFatal("Cannot load user home", err)
+
+	if len(os.Args) == 1 {
+		fmt.Println("Missing page parameter")
+		os.Exit(-1)
+	} else {
+		page := os.Args[1]
+		err := repo.Load(home + "/.cache/tldr/")
+		utils.HandleFatal("Cannot load repo", err)
+
+		fmt.Println(page)
+	}
 }
---
+++ repo/manager.go
@@ -0,0 +1,34 @@
+package repo
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+const (
+	REPO_PATH = "https://github.com/tldr-pages/tldr.git"
+)
+
+func Clone(repoDir string) (err error) {
+	cmd := exec.Command("git", "clone", REPO_PATH, repoDir)
+	err = cmd.Run()
+	return
+}
+
+func Update(repoDir string) {
+	// TODO
+}
+
+func IsCloned(repoDir string) bool {
+	_, err := os.Stat(repoDir)
+	return !os.IsNotExist(err)
+}
+
+func Load(repoDir string) error {
+	if !IsCloned(repoDir) {
+		fmt.Println("Repo not found, cloning to", repoDir)
+		return Clone(repoDir)
+	}
+	return nil
+}
