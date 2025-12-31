ae2e5765915fbd232034b47d39948090ba29abd0
Paulo <git@pauloo27.dev>
2025-12-31 19:33:11 -0300

    feat: nix flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Unofficial TL;DR pages client in Go";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          pname = "tldr";
+          version = "0.1.0";
+
+          src = ./.;
+
+          vendorHash = "sha256-+cn9NiNfsdlEteXV9MPZUf3BSLF6lFwLaKwMQls3S2A=";
+
+          ldflags = [ "-w" "-s" ];
+
+          subPackages = [ "cmd/tldr" ];
+
+          meta = with pkgs.lib; {
+            description = "Unofficial TL;DR pages client in Go";
+            homepage = "https://github.com/pauloo27/tldr";
+            license = licenses.mit;
+            maintainers = [ ];
+            mainProgram = "tldr";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+          ];
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/tldr";
+        };
+      }
+    );
+}
